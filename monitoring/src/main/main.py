bffa09cabac978350bf19431202314a44dd7cc99
chris chros <mrstreller@live.de>
2022-12-05 12:35:57 +0100

    alle benötigten Größen werden abgefragt

--- monitoring/src/main/main.py
+++ monitoring/src/main/main.py
@@ -3,6 +3,8 @@
 import time
 import RPi.GPIO as gpio
 
+import psutil
+
 from datetime import datetime as dt, timedelta
 
 curent_time: dt = dt.now()
@@ -24,8 +26,20 @@ gpio.setup(pinNumber, gpio.IN)
 
 while True:
     
+    print(f"CPU Temperatur: {psutil.sensors_temperatures(fahrenheit=False)['cpu_thermal'][0].current:.1f} °C")
+    print(f"CPU   Frequenz: {psutil.cpu_freq().current} MHz")
+    print(f"CPU       Last: {psutil.cpu_percent(interval=None)} %")
+    print(f"RAM Auslastung: {psutil.virtual_memory().percent} %")
+    print(f"RAM      total: {psutil.virtual_memory().total} bytes")
+    print(f"RAM  available: {psutil.virtual_memory().available} bytes")
+    print(f"WLAN  gesendet: {psutil.net_io_counters(nowrap=False).bytes_sent} bytes")
+    print(f"WLAN empfangen: {psutil.net_io_counters().bytes_recv} bytes")
+    print()
+    #break
+    time.sleep(1)
+    continue
     # I2C Pins schalten gegen Masse, also auf fallende Flanke warten
     gpio.wait_for_edge(pinNumber, gpio.FALLING)
     calc_power(dt.now())
-    
+   
 
